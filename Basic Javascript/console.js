967aa407d185ae5a169e3fec7070214c64451774
Ionut D <drionutamzn@gmail.com>
2021-01-13 02:49:53 +0000

    Module #1 - Submission #1

---
+++ Basic Javascript/console.js
@@ -0,0 +1,40 @@
+// console.log("just a console message");
+
+// window.alert("Hello window.alert");
+// document.write("Hello document.write");
+
+// var text = "Javascript string value";
+// document.write(text);
+
+// Assignment #7 Javascript
+// var annoyingPopUp = "unpleasant message";
+// window.alert(annoyingPopUp);
+
+// Assigment #8, #9 and Challenge #1
+// var trickyText =
+//   "In JavaScript, there are three ways to write a string — " +
+//   "they can be written inside single quotes (' '), double quotes (\" \"), or backticks (` `).";
+// document.write(trickyText);
+
+// Assignment  #10, #11
+// var nameIsDeprecated = "John",
+//   surname = "Doe",
+//   dateOfBirth = "12-12-1212";
+
+// var nameIsDeprecated = nameIsDeprecated.fontcolor("brown");
+// var surname = surname.fontcolor("blue");
+// var dateOfBirth = dateOfBirth.fontcolor("red");
+
+// document.write(surname);
+
+// Assignment #12
+// Expression
+// var expression = 5;
+
+// Assignment Submission # 1
+var nameIsDeprecated = "John", // an expression
+  surname = "Doe", // variables in one statement
+  surname = surname.fontcolor("blue"), // assigning a color to a variable
+  dateOfBirth = "12-12-1212";
+
+document.write(nameIsDeprecated + " " + surname); // using concatenation
