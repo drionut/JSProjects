6f17db8f0f76f5c819db532ca2d962fee81d3443
Ionut D <drionutamzn@gmail.com>
2021-01-20 09:59:52 +0000

    Add basic-dictionary files

---
+++ Basic Javascript/projects/Project4_dictionaries/js/main.js
@@ -0,0 +1,16 @@
+function Europe() {
+  var Country = {
+    // Name: "Kingdom of Denmark",
+    Name: "Denmark",
+    Capital: "Copenhagen",
+    Language: "Danish",
+    Religion: "Christianity",
+    Area: 42933,
+    Population: 5837213,
+    Currency: "EURO",
+  };
+
+  delete Country.Area;
+
+  document.getElementById("outputText").innerText = Country.Area;
+}
