814ca1e71d1b2971b8a0da3bf021b0bdeafc54c0
Ionut D <drionutamzn@gmail.com>
2021-03-02 17:56:27 +0000

    TicTacToe - add sounds/drawing winning lines

--- Basic Javascript/projects/tic-tac-toe/js/tictactoe.js
+++ Basic Javascript/projects/tic-tac-toe/js/tictactoe.js
@@ -25,6 +25,8 @@
 // END OF ARROW FUNCTION
 
 // TIC TAC TOE
+// msg var
+let message = document.getElementById("message");
 // score variables
 let player = document.getElementById("player_score");
 let computer = document.getElementById("computer_score");
@@ -44,6 +46,8 @@ function placeXOr0(squareNumber) {
     // get html element ID that was clicked
     let clicked = document.getElementById(squareNumber);
 
+    // clicked element and activePlayer are concatenated and added to array
+
     // checking player to place pictures
     if (activePlayer === "X") {
       clicked.style.backgroundImage = 'url("./images/x.png")';
@@ -51,37 +55,28 @@ function placeXOr0(squareNumber) {
       clicked.style.backgroundImage = 'url("./images/o.png")';
     }
 
-    // clicked element and activePlayer are concatenated and added to array
     selectedSquares.push(squareNumber + activePlayer);
-    console.log("All selected moves: " + selectedSquares);
-
-    // check for wins
-    console.log("checking for win conditions");
     checkWinConditions();
-
-    // condition for changing the activePlayer
-    if (activePlayer === "X") {
-      activePlayer = "O";
-    } else {
-      activePlayer = "X";
-    }
-
-    // call audio()
 
     // condition to check if it is computer turn
     // preventing autostart after first game
-    if (activePlayer === "O") {
-      if (selectedSquares.length == 0) {
-        activePlayer = "X";
-      } else {
+    if (activePlayer === "X" && selectedSquares.length > 0) {
+      activePlayer = "O";
+      if (activePlayer === "O") {
         disableClick();
         setTimeout(function () {
           computerTurn();
         }, 1000);
       }
+    } else {
+      activePlayer = "X";
     }
+
+    // call audio()
+    audio("./media/place.mp3");
   } else {
-    console.log("can't select " + squareNumber);
+    // message
+    message.innerHTML = "Can't select square " + (parseInt(squareNumber) + 1);
     return false;
   }
 
@@ -99,17 +94,17 @@ function computerTurn() {
   while (!success) {
     // generating a random number
     random = Math.round(Math.random() * 8);
-    console.log("Computer picked: " + random);
+
+    // message
+    message.innerHTML = "Computer picked square " + (random + 1);
 
     if (placeXOr0(random)) {
       success = true;
-    } else {
-      console.log("keep trying computer ... ");
     }
   }
 }
 
-// function checks if an array includes 3 strings
+// function checks if array includes 3 strings
 function arrayIncludes(squareA, squareB, squareC) {
   const a = selectedSquares.includes(squareA);
   const b = selectedSquares.includes(squareB);
@@ -127,141 +122,126 @@ function arrayIncludes(squareA, squareB, squareC) {
 function checkWinConditions() {
   // x - 0,1,2
   if (arrayIncludes("0X", "1X", "2X")) {
-    // drawWinLine(50, 100, 558, 100);
-    console.log("%c x wins top row ", "background: #222; color: #bada55");
+    message.innerHTML = "You win on top row";
     player_score = player_score + 1;
     player.innerHTML = player_score;
-    resetGame();
+    drawWinLine(50, 100, 558, 100);
   }
   // x - 3,4,5
   else if (arrayIncludes("3X", "4X", "5X")) {
-    // drawWinLine(50, 304, 558, 304);
-    console.log("%c x wins middle row ", "background: #222; color: #bada55");
+    message.innerHTML = "You win on middle row";
     player_score = player_score + 1;
     player.innerHTML = player_score;
-    resetGame();
+    drawWinLine(50, 304, 558, 304);
   }
   // x - 6,7,8
   else if (arrayIncludes("6X", "7X", "8X")) {
-    // drawWinLine(50, 508, 558, 508);
-    console.log("%c x wins bottom row ", "background: #222; color: #bada55");
+    message.innerHTML = "You win on bottom row";
     player_score = player_score + 1;
     player.innerHTML = player_score;
-    resetGame();
+    drawWinLine(50, 508, 558, 508);
   }
   // x - 0,3,6
   else if (arrayIncludes("0X", "3X", "6X")) {
-    // drawWinLine(100, 50, 100, 558);
-    console.log("%c x wins left vertical row", "background: #222; color: #bada55");
+    message.innerHTML = "You win on left-vertical row";
     player_score = player_score + 1;
     player.innerHTML = player_score;
-    resetGame();
+    drawWinLine(100, 50, 100, 558);
   }
   // x - 1,4,7
   else if (arrayIncludes("1X", "4X", "7X")) {
-    // drawWinLine(304, 50, 304, 558);
-    console.log("%c x wins middle vertical row ", "background: #222; color: #bada55");
+    message.innerHTML = "You win on middle-vertical row";
     player_score = player_score + 1;
     player.innerHTML = player_score;
-    resetGame();
+    drawWinLine(304, 50, 304, 558);
   }
   // x - 2,5,8
   else if (arrayIncludes("2X", "5X", "8X")) {
-    // drawWinLine(508, 50, 508, 558);
-    console.log("%c x wins rigth vertical row ", "background: #222; color: #bada55");
+    message.innerHTML = "You win on right-vertical row";
     player_score = player_score + 1;
     player.innerHTML = player_score;
-    resetGame();
+    drawWinLine(508, 50, 508, 558);
   }
   // x - 6,4,2
   else if (arrayIncludes("6X", "4X", "2X")) {
-    // drawWinLine(100, 508, 510, 90);
-    console.log("%c x wins diagonal row 6-4-2 ", "background: #222; color: #bada55");
+    message.innerHTML = "You win on diagonal row 7-5-3";
     player_score = player_score + 1;
     player.innerHTML = player_score;
-    resetGame();
+    drawWinLine(100, 508, 510, 90);
   }
   // x - 0,4,8
   else if (arrayIncludes("0X", "4X", "8X")) {
-    // drawWinLine(100, 100, 520, 520);
-    console.log("%c x wins diagonal row 0-4-8", "background: #222; color: #bada55");
+    message.innerHTML = "You win on diagonal row 1-5-9";
     player_score = player_score + 1;
     player.innerHTML = player_score;
-    resetGame();
+    drawWinLine(100, 100, 520, 520);
   }
   //   to implement checks for O - computer score
   //   O - 0-1-2
   else if (arrayIncludes("0O", "1O", "2O")) {
-    // drawWinLine(50, 100, 558, 100);
-    console.log("%c O wins top row ", "background: #222; color: #bada55");
+    message.innerHTML = "Computer win on top row";
     computer_score = computer_score + 1;
     computer.innerHTML = computer_score;
-    resetGame();
+    drawWinLine(50, 100, 558, 100);
   }
   // O - 3,4,5
   else if (arrayIncludes("3O", "4O", "5O")) {
-    // drawWinLine(50, 304, 558, 304);
-    console.log("%c O wins middle row ", "background: #222; color: #bada55");
+    message.innerHTML = "Computer win on middle row";
     computer_score = computer_score + 1;
     computer.innerHTML = computer_score;
-    resetGame();
+    drawWinLine(50, 304, 558, 304);
   }
   // O - 6,7,8
   else if (arrayIncludes("6O", "7O", "8O")) {
-    // drawWinLine(50, 508, 558, 508);
-    console.log("%c O wins bottom row ", "background: #222; color: #bada55");
+    message.innerHTML = "Computer win on bottom row";
     computer_score = computer_score + 1;
     computer.innerHTML = computer_score;
-    resetGame();
+    drawWinLine(50, 508, 558, 508);
   }
   // O - 0,3,6
   else if (arrayIncludes("0O", "3O", "6O")) {
-    // drawWinLine(100, 50, 100, 558);
-    console.log("%c O wins left vertical row", "background: #222; color: #bada55");
+    message.innerHTML = "Computer win on left-vertical row";
     computer_score = computer_score + 1;
     computer.innerHTML = computer_score;
-    resetGame();
+    drawWinLine(100, 50, 100, 558);
   }
   // O - 1,4,7
   else if (arrayIncludes("1O", "4O", "7O")) {
-    // drawWinLine(304, 50, 304, 558);
-    console.log("%c O wins middle vertical row ", "background: #222; color: #bada55");
+    message.innerHTML = "Computer win on middle-vertical row";
     computer_score = computer_score + 1;
     computer.innerHTML = computer_score;
-    resetGame();
+    drawWinLine(304, 50, 304, 558);
   }
   // O - 2,5,8
   else if (arrayIncludes("2O", "5O", "8O")) {
-    // drawWinLine(508, 50, 508, 558);
-    console.log("%c O wins rigth vertical row ", "background: #222; color: #bada55");
+    message.innerHTML = "Computer win on right-vertical row";
     computer_score = computer_score + 1;
     computer.innerHTML = computer_score;
-    resetGame();
+    drawWinLine(508, 50, 508, 558);
   }
   // O - 6,4,2
   else if (arrayIncludes("6O", "4O", "2O")) {
-    // drawWinLine(100, 508, 510, 90);
-    console.log("%c O wins diagonal row 6-4-2 ", "background: #222; color: #bada55");
+    message.innerHTML = "Computer win on diagonal row 7-5-3";
     computer_score = computer_score + 1;
     computer.innerHTML = computer_score;
-    resetGame();
+    drawWinLine(100, 508, 510, 90);
   }
   // O - 0,4,8
   else if (arrayIncludes("0O", "4O", "8O")) {
-    // drawWinLine(100, 100, 520, 520);
-    console.log("%c O wins diagonal row 0-4-8", "background: #222; color: #bada55");
+    message.innerHTML = "Computer win on diagonal row 1-5-9";
     computer_score = computer_score + 1;
     computer.innerHTML = computer_score;
-    resetGame();
+    drawWinLine(100, 100, 520, 520);
   }
 
   //
   else if (selectedSquares.length >= 9) {
-    console.clear();
-    console.log("%c is a tie - reset in 2 seconds ", "background: #222; color: #bada55");
-    setTimeout(function () {
-      resetGame();
-    }, 1000);
+    resetGame();
+    audio("./media/tie.mp3");
+    setTimeout(() => {
+      clear();
+    }, 2000);
+    message.innerHTML = "Is a tie! - resetting the game.";
   }
 }
 
@@ -276,8 +256,86 @@ function disableClick() {
   }, 1000);
 }
 
+// function takes a string parameter of the path for audio files
+function audio(audioURL) {
+  // creating an audio object
+  let audio = new Audio(audioURL);
+  // play our audio
+  audio.play();
+}
+
+// drawing win lines
+function drawWinLine(coordsStartX, coordsStartY, coordsEndX, coordsEndY) {
+  const canvas = document.getElementById("win-lines");
+  const ctx = canvas.getContext("2d");
+
+  // declaring variables to be using when checking for endpoints
+  let startX = coordsStartX,
+    startY = coordsStartY;
+
+  // animating the line
+  function animateLineDrawing() {
+    const animation = requestAnimationFrame(animateLineDrawing);
+    ctx.beginPath();
+    ctx.moveTo(coordsStartX, coordsStartY);
+    ctx.lineTo(startX, startY);
+    // testing purposes line - TO BE DELETED
+    // ctx.lineTo(coordsEndX, coordsEndY);
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = "rgba(70,255,33,.8)";
+    ctx.stroke();
+
+    //   condition to check if we reached endpoint for vertically/horizontally
+    if (startX <= coordsEndX && startY <= coordsEndY) {
+      if (startX < coordsEndX) {
+        startX += 5;
+      }
+      if (startY < coordsEndY) {
+        startY += 5;
+      }
+      if (startX >= coordsEndX && startY >= coordsEndY) {
+        // stop the animateLineDrawing and clear the canvas
+        cancelAnimationFrame(animation);
+        ctx.clearRect(0, 0, 608, 608);
+        setTimeout(() => {
+          clear();
+        }, 1500);
+      }
+    }
+
+    // condition to check for diagonally 6-4-2
+    if (startX <= coordsEndX && startY >= coordsEndY) {
+      if (startX < coordsEndX) {
+        startX += 5;
+      }
+      if (startY > coordsEndY) {
+        startY -= 5;
+      }
+      if (startX >= coordsEndX && startY <= coordsEndY) {
+        // stop the animateLineDrawing and clear the canvas
+        cancelAnimationFrame(animation);
+        ctx.clearRect(0, 0, 608, 608);
+        setTimeout(() => {
+          clear();
+        }, 1500);
+      }
+    }
+  }
+
+  disableClick();
+  audio("./media/winGame.mp3");
+  animateLineDrawing();
+  resetGame();
+}
+
 // reset the game if a tie or win
 function resetGame() {
+  // reset array with moves
+  selectedSquares = [];
+}
+
+// clearing the table
+function clear() {
   // loop through each HTML square element
   for (let i = 0; i < 9; i++) {
     // gets the HTML element of i
@@ -285,7 +343,5 @@ function resetGame() {
     // removes background images
     square.style.backgroundImage = "";
   }
-  // reset array with moves
-  selectedSquares = [];
-  //   console.clear();
+  message.innerHTML = "Your turn. Place an X.";
 }
