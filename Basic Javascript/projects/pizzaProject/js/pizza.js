f9f1f41effe75393a4e17b77525f22314ba14edd
Ionut D <drionutamzn@gmail.com>
2021-03-29 23:09:13 +0100

    Fix veggies and toppings count

--- Basic Javascript/projects/pizzaProject/js/pizza.js
+++ Basic Javascript/projects/pizzaProject/js/pizza.js
@@ -14,62 +14,68 @@ function getReceipt() {
     if (selectedSize === "Personal Pizza") {
         sizeTotal = 6;
     } else if (selectedSize === "Small Pizza") {
-        sizeTotal = 4;
-    } else if (selectedSize === "Medium Pizza") {
         sizeTotal = 8;
+    } else if (selectedSize === "Medium Pizza") {
+        sizeTotal = 10;
     } else if (selectedSize === "Large Pizza") {
-        selectedSize = 10;
+        selectedSize = 14;
     } else if (selectedSize === "Extra Large Pizza") {
-        sizeTotal = 12;
+        sizeTotal = 16;
     }
     runningTotal = sizeTotal;
     getVegetables(runningTotal, text);
 }
 
 function getVegetables(runningTotal, text) {
-    var vegetableTotal = 0;
-    var selectedVegetable = [];
+    // var vegetableTotal = 0;
+    // var selectedVegetable = [];
+    var veggiesCount = 0;
     var vegetableArray = document.getElementsByClassName("vegetables");
 
     for (var i = 0; i < vegetableArray.length; i++) {
         if (vegetableArray[i].checked) {
-            selectedVegetable.push(vegetableArray[i].value);
+            // selectedVegetable.push(vegetableArray[i].value);
+            veggiesCount += 1;
             text = text + vegetableArray[i].value + "<br>";
         }
     }
 
-    var vegetableCount = selectedVegetable.length;
-    if (vegetableTotal > 1) {
-        vegetableTotal = vegetableCount - 1;
-    } else {
-        vegetableCount = 0;
-    }
+    // Sorry - No discount on veggies this year due to COVID 19
 
-    runningTotal = runningTotal + vegetableTotal;
+    // var vegetableCount = selectedVegetable.length;
+    // if (vegetableTotal > 1) {
+    //     vegetableTotal = vegetableCount - 1;
+    // } else {
+    //     vegetableCount = 0;
+    // }
 
+    runningTotal = runningTotal + veggiesCount;
     getTopping(runningTotal, text);
 }
 
 function getTopping(runningTotal, text) {
-    var toppingTotal = 0;
-    var selectedTopping = [];
+    var toppingCount = 0;
+    // var selectedTopping = [];
     var toppingArray = document.getElementsByClassName("toppings");
 
     for (var i = 0; i < toppingArray.length; i++) {
         if (toppingArray[i].checked) {
-            selectedTopping.push(toppingArray[i].value);
+            // selectedTopping.push(toppingArray[i].value);
             text = text + toppingArray[i].value + "<br>";
+            toppingCount += 1;
         }
     }
 
-    var toppingCount = selectedTopping.length;
-    if (toppingTotal > 1) {
-        toppingTotal = toppingCount - 1;
+    // var toppingCount = selectedTopping.length;
+
+    // some discount
+    if (toppingCount > 1) {
+        toppingCount = toppingCount - 1;
     } else {
-        toppingTotal = 0;
+        toppingCount = 0;
     }
 
-    runningTotal = runningTotal + toppingTotal;
+    runningTotal = runningTotal + toppingCount;
 
     document.getElementById("showText").innerHTML = text;
     document.getElementById("totalPrice").innerHTML = "<h3>Total: <strong>$" + runningTotal + ".00" + "</strong></h3>";
