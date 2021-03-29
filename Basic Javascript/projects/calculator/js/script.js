c7676c39dd5f0435a7cb6365336340bca1f6eb59
Ionut D <drionutamzn@gmail.com>
2021-03-29 17:49:59 +0100

    Update - calculator markup

---
+++ Basic Javascript/projects/calculator/js/script.js
@@ -0,0 +1,11 @@
+// creating object
+const Calculator = {
+  displayValue: "0",
+  firstOperand: null,
+  waitSecondOperand: false,
+  operator: null,
+};
+
+function inputDigit(digit) {
+  const { displayValue, waitSecondOperand } = Calculator;
+}
